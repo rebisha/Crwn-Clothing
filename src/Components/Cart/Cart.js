a1763ca4e813597825cdc0e3aeb794f0b8e825a2
Rebisha Pokharel <rebisha.pokharel@isobar.com>
2020-06-01 11:16:34 +1000

    - adding itemCount to Cart component.

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,28 +1,36 @@
 // global modules
 import React from "react";
 import { connect } from "react-redux";
-import { func } from "prop-types";
+import { func, object } from "prop-types";
 // components
 import CartIcon from "../Icons/CartIcon";
 import { toggleCartHidden } from "../../Redux/Cart/action";
 // styles
 import "./cart.scss";
 
-const Cart = ({ toggleCartHidden }) => {
+const Cart = ({ toggleCartHidden, itemCount }) => {
   return (
     <div className="cart-wrapper" onClick={toggleCartHidden}>
       <CartIcon />
-      <span className="item-count"> 0 </span>
+      <span className="item-count"> {itemCount} </span>
     </div>
   );
 };
 
 Cart.propTypes = {
-  toggleCartHidden: func
+  toggleCartHidden: func,
+  itemCount: object
 };
 
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(Cart);
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+    0
+  )
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
