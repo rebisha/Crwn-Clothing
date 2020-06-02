cb00dad0fc707e7847af2f9ab065c867653e8890
Rebisha Pokharel <rebisha.pokharel@isobar.com>
2020-06-02 19:06:19 +1000

    - using redux-persist to store cart state/items in the local storage for web

--- src/Redux/root-reducer.js
+++ src/Redux/root-reducer.js
@@ -1,10 +1,20 @@
 // global modules
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // this line is basically telling a persist to use local storage as my default storage. defaults to localStorage for web
 // reducers
 import userReducer from "./User/reducer";
 import cartReducer from "./Cart/reducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"]
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
